7f7f03376848ab307c728830f06431df026cf835
Ciprian Ciomag <ciprianciomag@Ciprians-Mac-mini.local>
2022-11-16 12:04:05 +0200

    Finished Gatsby Tutorial Part 3

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,5 @@ module.exports = {
     title: `Gatsby`,
     siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: [],
-}
+  plugins: ["gatsby-plugin-image", "gatsby-plugin-sharp"],
+};
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,29 +1,16 @@
-// Step 1: Import React
-// import * as React from "react";
-// import { Link } from "gatsby";
-
-// const IndexPage = () => {
-//   return (
-//     <main>
-//       <h1>Welcome to my Gatsby site!</h1>
-//       <Link to="/about">About</Link>
-//       <p>I'm making this by following the Gatsby Tutorial.</p>
-//     </main>
-//   );
-// };
-
-// export const Head = () => <title>Home Page</title>;
-
-// export default IndexPage;
-
 import * as React from "react";
 import Layout from "../components/layout";
+import { StaticImage } from "gatsby-plugin-image";
 
 const IndexPage = () => {
   return (
     <Layout pageTitle="Home Page">
       <h2>Welcome to my Gatsby Site!</h2>
       <p>I'm making this by following the Gatsby Tutorial.</p>
+      <StaticImage
+        alt="Clifford, a reddish-brown pitbull, posing on a couch and looking stoically at the camera"
+        src="https://pbs.twimg.com/media/E1oMV3QVgAIr1NT?format=jpg&name=large"
+      />
     </Layout>
   );
 };
